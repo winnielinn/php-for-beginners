9085a2fee0143dcab9c7990bb799118e68738a78
winnielinn <yaysa903@gmail.com>
2024-02-20 10:24:25 +0800

    feat: add form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -4,12 +4,24 @@ $heading = 'Create Note';
 $config = require('config.php');
 $db = new Database($config);
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
-        ':body' => $_POST['body'],
-        ':user_id' => 4
-    ]);
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'Body is required';
+    }
+
+    if (strlen($_POST['body']) > 100) {
+        $errors['body'] = 'Body must be less than 100 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+            ':body' => $_POST['body'],
+            ':user_id' => 4
+        ]);
+    }
 }
 
 require('views/note-create.view.php');
-
