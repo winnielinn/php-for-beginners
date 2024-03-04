ea6f3d38e1465420794cd340cdbbed842b7cb0ee
winnielinn <yaysa903@gmail.com>
2024-03-05 00:15:09 +0800

    feat: manage passwords like this

--- controllers/register/store.php
+++ controllers/register/store.php
@@ -40,7 +40,7 @@ if ($result) {
 } else {
     $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT),
     ]);
 
     $_SESSION['user'] = [
